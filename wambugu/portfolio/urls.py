0d40f65d4b3fd2b479d360919e42e61cd02567b2
Polymathh <wambugum110@gmail.com>
2024-12-29 09:26:52 +0300

    initial commit

--- wambugu/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='Home'),
-    path('about/', views.about, name='About'),
-    path('projects/', views.project, name='Projects'),
-    path('blog/', views.blog, name='blog'),
-]
